850bb9d3068c6eff8ec616ff900965b80728a396
Michael MacDonald <michael@starclass.com.au>
2011-03-31 11:53:28 +1100

    Removed rails from the equation by using dbi connection to test for the existence of a database so that the switch is quicker

--- git_switch_database.rb
+++ git_switch_database.rb
@@ -1,19 +1,27 @@
 # check if the development database for this branch exists and create it if not
 
-database_configuration = ActiveRecord::Base.configurations["development"]
-database_name = database_configuration["database"]
+require 'yaml'
+require 'erb'
 
-ActiveRecord::Base.establish_connection(database_configuration)
+db_settings = YAML.load(ERB.new(File.read("config/database.yml")).result)["development"]
+db_name = db_settings["database"]
+db_host = "localhost"
+db_user = db_settings["username"]
+db_pass = db_settings["password"]
+
+require 'dbi'
+require 'dbd/mysql'
+
 begin
-  ActiveRecord::Base.connection
-  puts "Switched to database '#{database_name}'"
-rescue
-  # create the database
-  puts "Creating the #{database_name} database"
-  %x[rake db:setup]
+  dbh = DBI.connect("DBI:Mysql:#{db_name}:#{db_host}", db_user, db_pass)
+  puts "Switched to database '#{db_name}'"
+rescue DBI::DatabaseError => e
+  puts "Database #{db_name} does not exist."
+  # puts "Creating the #{db_name} database"
+  # %x[rake db:setup]
 end
 
 # now we know we have a database, setup the test database
 
-puts "Preparing the test database"
-%x[rake db:test:prepare]
+# puts "Preparing the test database"
+# {}%x[rake db:test:prepare]
